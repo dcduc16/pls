8d4281fa836a23de3075e03ff36d12f98e895c9a
duduc168 <duccdinh16@gmail.com>
2023-10-15 15:36:04 +0700

    first commit

--- server.php
+++ server.php
@@ -1,6 +1,25 @@
 <?php
 
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="shell.php"');
+error_reporting(0);
+$conn = mysqli_connect('db', 'user', 'test', "dockerExample");
 
-readfile("shell.php");
+
+$query = 'SELECT * From Person';
+$result = mysqli_query($conn, $query);
+
+echo '<table class="table table-striped">';
+echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+while ($value = $result->fetch_array(MYSQLI_ASSOC)) {
+    echo '<tr>';
+    echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+    foreach ($value as $element) {
+        echo '<td>' . $element . '</td>';
+    }
+
+    echo '</tr>';
+}
+echo '</table>';
+
+$result->close();
+
+mysqli_close($conn);
