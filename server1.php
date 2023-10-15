e2b2cb56d1508ce228739fcfbe23a2acfb0d0ea3
duduc168 <duccdinh16@gmail.com>
2023-10-15 16:01:06 +0700

    first commit

--- server1.php
+++ server1.php
@@ -3,5 +3,22 @@ error_reporting(0);
 $conn = mysqli_connect('db', 'user', 'test', "dockerExample");
 
 
-$query = 'SELECT * From Person';
+$query = 'SELECT * From Flag_is_secret';
 $result = mysqli_query($conn, $query);
+
+echo '<table class="table table-striped">';
+echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+while ($value = $result->fetch_array(MYSQLI_ASSOC)) {
+    echo '<tr>';
+    echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+    foreach ($value as $element) {
+        echo '<td>' . $element . '</td>';
+    }
+
+    echo '</tr>';
+}
+echo '</table>';
+
+$result->close();
+
+mysqli_close($conn);
